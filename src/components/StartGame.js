99ee83cd47263596ec5f4e61af0c933f7df05e80
Abu Zaid Khan <abuzaidkhan99@gmail.com>
2022-03-17 22:56:28 -0400

    Generating 20 movies now

--- src/components/StartGame.js
+++ src/components/StartGame.js
@@ -2,14 +2,15 @@ import React, { Component, useState } from 'react'
 
 export const StartGame = (props) => {
     const API_KEY = process.env.REACT_APP_MOVIE_API_KEY
-    const TMDB_API = "https://api.themoviedb.org/3/movie/550?api_key="
+    const TMDB_API = "https://api.themoviedb.org/3/discover/movie?api_key="+API_KEY+"&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=true&page=1&with_watch_monetization_types=flatrate"
 
     const getData = () => {
         props.flipper();
-        fetch(TMDB_API + API_KEY).then(res => res.json())
+        /*fetch(TMDB_API).then(res => res.json())
             .then(data => {
-                console.log(data.status);
-            })
+                var movies = data.results
+                console.log(movies[1]);
+            })*/
     }
 
     return(
